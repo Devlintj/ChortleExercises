be2a030225624d58a0e1c6b292b416de2402f0d8
Tyler James Devlin <devlintj@dcsdk12.org>
2017-02-22 12:34:32 -0700

    Exercise 30_2

---
+++ Exercise30_2.java
@@ -0,0 +1,7 @@
+
+public class Exercise30_2 {
+	public static void main(String[] args) {
+		Exercise30_2_Tester object = new Exercise30_2_Tester("good morning", "good evening");
+		object.speak();
+	}
+}
