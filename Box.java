33429a1a12564f82cbd7308029b505dcead99b59
Tyler James Devlin <devlintj@dcsdk12.org>
2017-03-03 12:56:42 -0700

    started exercise

---
+++ Box.java
@@ -0,0 +1,16 @@
+
+public class Box {
+	Box(double width, double height, double length) {
+		
+	}
+	Box(double side) {
+		
+	}
+	
+	double volume() {
+		
+	}
+	double area() {
+		
+	}
+}
---
+++ Exercise33_1.java
@@ -0,0 +1,6 @@
+
+public class Exercise33_1 {
+	public static void main(String[] args) {
+		
+	}
+}
