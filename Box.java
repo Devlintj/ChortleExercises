ae0d40f7f9eeb186f4114dd981e7337735e4cebd
Tyler James Devlin <devlintj@dcsdk12.org>
2017-03-07 12:43:34 -0700

    Exercise 33_1

--- Box.java
+++ Box.java
@@ -1,16 +1,35 @@
 
 public class Box {
+	private double width, height, length;
 	Box(double width, double height, double length) {
-		
+		this.width = width;
+		this.height = height;
+		this.length = length;
 	}
 	Box(double side) {
-		
+		width = side;
+		height = side;
+		length = side;
 	}
 	
 	double volume() {
-		
+		double volume = width * height * length;
+		return volume;
 	}
 	double area() {
-		
+		double area = (width * length * 2) + (width * height * 2) + (height * length * 2);
+		return area;
+	}
+	
+	double length() {
+		return length;
+	}
+	
+	double height() {
+		return height;
+	}
+	
+	double width() {
+		return width;
 	}
 }
